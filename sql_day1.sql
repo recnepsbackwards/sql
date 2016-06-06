05df9114a9779e5bef0d98b1a334ba736b0e09d6
Spencer West <spencer.west.zbt@gmail.com>
2016-06-06 15:31:15 -0700

    initial commit

---
+++ sql_day1.sql
@@ -0,0 +1,13 @@
+﻿SELECT  code, name, population, surfacearea, gnp, population / gnp AS pop_gnp_ratio, surfacearea / gnp AS area_gnp_ratio
+FROM countries
+WHERE gnp > 0 
+--ORDER BY area_gnp_ratio ASC 
+--LIMIT 10
+
+--INTERSECT
+
+--SELECT  code, name, population, surfacearea, gnp, population / gnp AS pop_gnp_ratio, surfacearea / gnp AS area_gnp_ratio 
+--FROM countries
+
+ORDER BY pop_gnp_ratio ASC 
+LIMIT 10
---
+++ sql_practice.sql
@@ -0,0 +1,8 @@
+﻿with 
+	indep_year as 
+(SELECT name, indepyear, governmentform
+From countries
+where indepyear > 1910 AND governmentform = 'Republic')
+
+SELECT count(name) from indep_year;
+
